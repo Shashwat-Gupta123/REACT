d6d69d77488f7ab3002fbcb29ebed55983ad390c
Shashwat Gupta <shashwatgupta062@gmail.com>
2024-06-22 16:07:04 +0530

    reacts hooks and api uses

---
+++ CurrencyConverter/currenyConverter/src/componenets/index.js
@@ -0,0 +1,3 @@
+import InputBox from "./InputBox";
+export {InputBox}
+// use these type of import when there and variable componenets
---
+++ CurrencyConverter/currenyConverter/src/hooks/usecurrencyinfo.js
@@ -0,0 +1,12 @@
+import { useState,useEffect } from "react";
+
+function useCurrencyinfo(currency){
+    const [data,setdata]=useState({})
+    useEffect(()=>{
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`)
+        .then((res)=>res.json())
+        .then((res)=>setdata(res[currency]))
+    },[currency])
+    return data;
+}
+export default useCurrencyinfo;
